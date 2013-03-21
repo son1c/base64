be6ca09434bd68341a8c6e3ac4261b4cf563b276
son1c <xsonicx2000@yahoo.de>
2013-03-21 23:35:56 +0100

    add utf-8 decoding

--- b64.py
+++ b64.py
@@ -14,19 +14,16 @@ try :
 	
 	if mode == "encode":
 	
-		encoded_str = str(base64.b64encode(wert.encode('ascii')))
+		encoded_str = str(base64.b64encode(wert.encode()))
 		tmp = encoded_str[2:]
 		lgtmp2 = len(tmp)
 		final = tmp[:lgtmp2-1]
 		print (final)
 	
 	if mode == "decode":
-	
-		decoded_str = str(base64.b64decode(wert))
-		tmp = decoded_str[2:]
-		lgtmp2 = len(tmp)
-		final = tmp[:lgtmp2-1]
-		print (final)
+		bwert = bytes(wert, 'utf-8')
+		decoded_str = base64.b64decode(bwert).decode()
+		print (decoded_str)
 		
 except :
 	print ("b64.py [encode/decode] [String]")
